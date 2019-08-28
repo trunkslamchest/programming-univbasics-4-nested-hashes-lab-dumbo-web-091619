e4819dd54f8ab5513187045fa601ebdf3efc7f14
Austin Smith <trunk9slamchest@gmail.com>
2019-08-28 23:39:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -125,7 +125,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-    add = programmer_hash[:dennis_ritchie][:languages] = 'Assembly'
+    add = programmer_hash[:dennis_ritchie][:languages] += 'Assembly'
 
   return programmer_hash
 end
