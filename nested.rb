d079eb37452bd1463460384bfb3d1f325eaa68d2
Austin Smith <trunk9slamchest@gmail.com>
2019-08-28 23:44:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,3 @@
-
 def hopper
 	programmer_hash = 
  		{
@@ -20,8 +19,6 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -37,7 +34,6 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-  
   return programmer_hash[:alan_kay][:known_for]
 end
 
@@ -57,7 +53,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     
+
   return programmer_hash[:dennis_ritchie][:languages][0]
 end
 
@@ -81,8 +77,6 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
 end
 
 def changing_alan
@@ -101,13 +95,10 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = programmer_hash[:alan_kay][:known_for] = "GUI"
-     
-     return programmer_hash
-end
 
-# puts changing_alan[:alan_kay][:known_for]
+   alans_new_info = programmer_hash[:alan_kay][:known_for] = "GUI"
+   return programmer_hash
+end
 
 def adding_to_dennis
 	programmer_hash = 
@@ -125,6 +116,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-    add = programmer_hash[:dennis_ritchie][:languages] += ['Assembly']
+     
+  add = programmer_hash[:dennis_ritchie][:languages] += ['Assembly']
   return programmer_hash
 end
