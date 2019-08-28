61a54b0816264da239c600ef5177eb13d0841077
Austin Smith <trunk9slamchest@gmail.com>
2019-08-28 23:45:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,5 +1,5 @@
 def hopper
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -19,7 +19,7 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -39,7 +39,7 @@ def alan_kay_is_known_for
 end
 
 def dennis_ritchies_language
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -81,7 +81,7 @@ def adding_matz
 end
 
 def changing_alan
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -102,7 +102,7 @@ def changing_alan
 end
 
 def adding_to_dennis
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -117,7 +117,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     
+
   add = programmer_hash[:dennis_ritchie][:languages] += ['Assembly']
   return programmer_hash
 end
